b092fd23d5ba4c9833a47502dac6d58ac2d623fe
Dave Coleman <services@theagsc.com>
2015-03-07 13:04:52 -0500

    removing wordpress! yay

--- web/app/plugins/roots-share-buttons/lib/activation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Roots\ShareButtons\Activation;
-
-if (get_option('roots_share_buttons') === false) {
-  add_option('roots_share_buttons', \Roots\ShareButtons\Admin\get_defaults());
-}
