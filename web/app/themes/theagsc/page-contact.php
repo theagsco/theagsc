637b5c6e9e33e48c9fa7c3243041890a141624f8
Dave Coleman <services@theagsc.com>
2016-02-19 11:15:55 +1100

    Updated travis

--- web/app/themes/theagsc/page-contact.php
+++ web/app/themes/theagsc/page-contact.php
@@ -3,7 +3,7 @@
 
 	<article>
 	    <div class="entry-content">
-			<p>It's our dream one day to open up a brick & mortar studio so that you can pop in and visit. Until then, email is a wonderful way to get in touch with us.</p>
+			<p>It's our dream one day to open up a brick &amp; mortar studio so that you can pop in and visit. Until then, email is a wonderful way to get in touch with us.</p>
 
 			<p>Do you have a project you'd like to work on with us? Ideas for tutorials or other content for the Community? Even if it's a simple hello or a funny gif, hearing from you makes our day. Go ahead and click the button below to email us.</p>
 
