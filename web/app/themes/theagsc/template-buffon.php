ee4852eea26f8ec3554cd59fc7c691cf225ff879
Dave Coleman <services@theagsc.com>
2016-02-27 14:47:46 +1100

    Styled main Buffon title, with drops and bursts

--- web/app/themes/theagsc/template-buffon.php
+++ web/app/themes/theagsc/template-buffon.php
@@ -3,17 +3,14 @@
  * Template Name: Buffon
  */
 ?>
-
-<p class="islaScript">Introducing</p>
-<h1 class="buffonTitle">Buffon</h1>
-<div id="burst">
-  <img class="svg" id="red" src="<?php echo get_template_directory_uri(); ?>/dist/images/drop.svg">
-  <img class="svg" id="yellow" src="<?php echo get_template_directory_uri(); ?>/dist/images/drop.svg">
-  <img class="svg" id="blue" src="<?php echo get_template_directory_uri(); ?>/dist/images/drop.svg">
-  <img class="svg" id="pink" src="<?php echo get_template_directory_uri(); ?>/dist/images/drop.svg">
+<div id="buffonTitleWrapper">
+  <p class="islaScript">Introducing</p>
+  <h1 class="buffonTitle" contenteditable="true">Buffon</h1>
+  <img class="svg red" id="redDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
+  <img class="svg yellow" id="yellowDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
+  <img class="svg blue" id="blueDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
+  <img class="svg pink" id="pinkDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
+  <img class="svg white" id="whiteDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
+  <img class="svg blue" id="blueBurst" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_burst.svg">
+  <img class="svg pink" id="pinkBurst" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_burst.svg">
 </div>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
