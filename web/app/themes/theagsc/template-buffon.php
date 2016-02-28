e0b9323c4537e758f1e20f97bbf9e5be752afa95
Dave Coleman <services@theagsc.com>
2016-02-28 22:06:37 +1100

    Finally got svg's and images to load using BrowserSync on iOS. There was a problem with the way I was referencing them in the code. Changed the title graphic from text and svg's to one big svg.

--- web/app/themes/theagsc/template-buffon.php
+++ web/app/themes/theagsc/template-buffon.php
@@ -6,16 +6,10 @@
 <a href="<?= esc_url(home_url('/')); ?>" class="theagsc_logo"><img class="svg" id="pinkBurst" src="<?php echo get_template_directory_uri(); ?>/dist/images/theagsc_small.svg"></a>
 <section id="buffonTitleWrapper">
   <p class="islaScript">Introducing</p>
-  <h1 class="buffonTitle"><span contenteditable="true">Buffon</span>
-    <img class="svg blue" id="blueBurst" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_burst.svg">
-    <img class="svg pink" id="pinkBurst" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_burst.svg">
-    <img class="svg red" id="redDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
-    <img class="svg yellow" id="yellowDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
-    <img class="svg blue" id="blueDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
-    <img class="svg pink" id="pinkDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
-    <img class="svg white" id="whiteDrop" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_drop.svg">
-  </h1>
+  <img class="svg" id="buffonTitle" src="<?php bloginfo('template_directory'); ?>/assets/images/buffon_title.svg">
 </section>
+
+<img class="svg" src="<?php bloginfo('template_directory'); ?>/assets/images/icons_hamburger.svg"/>
 
 <section id="blurb">
 <p contenteditable="true">A playful reverse-contrast typeface from <strong>The AGSC</strong>.</p>
@@ -23,11 +17,11 @@
 
 <section id="weights">
   <p class="thin" contenteditable="true">In a light-footed, delicate Thin</p>
-  <img class="svg yellow" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_burst.svg">
+  <img class="svg yellow" src="<?php echo get_template_directory_uri(); ?>/assets/images/buffon_burst.svg">
   <p class="regular" contenteditable="true">A spirited, friendly Regular</p>
-  <img class="svg yellow" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_burst.svg">
+  <img class="svg yellow" src="<?php echo get_template_directory_uri(); ?>/assets/images/buffon_burst.svg">
   <p class="bold" contenteditable="true">A hearty, big-boned Bold</p>
-  <img class="svg yellow" src="<?php echo get_template_directory_uri(); ?>/dist/images/buffon_burst.svg">
+  <img class="svg yellow" src="<?php echo get_template_directory_uri(); ?>/assets/images/buffon_burst.svg">
   <p class="heavy" contenteditable="true">&amp; a fat, sassy Heavy.</p>
 </section>
 
